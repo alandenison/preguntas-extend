4301394b23d6930319f4d029838dc7dfe964b071
Alan Denison <alan79td@gmail.com>
2016-08-29 11:10:04 -0700

    add helper to show questions without answers

---
+++ app/helpers/sin-respuesta.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function sinRespuesta(params) {
+  var pregunta = params[0];
+
+  if(pregunta.get('answers').get('length') === 0) {
+    return Ember.String.htmlSafe('<span>¡no hay respuesta!</span>');
+  }
+}
+
+export default Ember.Helper.helper(sinRespuesta);
---
+++ tests/unit/helpers/sin-respuesta-test.js
@@ -0,0 +1,10 @@
+import { sinRespuesta } from 'questions/helpers/sin-respuesta';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | sin respuesta');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = sinRespuesta([42]);
+  assert.ok(result);
+});
